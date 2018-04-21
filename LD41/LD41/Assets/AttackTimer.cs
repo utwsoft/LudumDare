bb04dac2e4f1cd74b6715c905b506fb931c5a2d1
a_a <aa@undertheweathersoftware.com>
2018-04-21 16:23:40 -0700

    Attack timer experiments, and muzzle flash
    Badguy attacking. muzzle flash init is a little wonky. flash is on sometimes at start.

---
+++ LD41/LD41/Assets/AttackTimer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackTimer : MonoBehaviour {
+
+    SpriteRenderer spriteRenderer;
+    public Sprite[] Sprites;
+
+    float curTime = 0.0f;
+    public float FrameTime = 0.1875f;
+
+    int frameIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+
+        SetSprite(Sprites[0]);
+	}
+	
+    
+	// Update is called once per frame
+	void Update () {
+
+        curTime += Time.deltaTime;
+
+        if (curTime > FrameTime)
+        {
+            frameIndex++;
+            frameIndex %= Sprites.Length;
+
+            curTime = 0.0f;
+
+            SetSprite(Sprites[frameIndex]);
+        }
+	}
+
+    void SetSprite(Sprite sprite)
+    {
+        spriteRenderer.sprite = sprite;
+    }
+}
---
+++ LD41/LD41/Assets/Badguy.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Badguy : MonoBehaviour {
+
+    GameObject MuzzleFlash1;
+    GameObject MuzzleFlash2;
+
+    public float curTime = 0.0f;
+    public float curFlashTime = 0.0f;
+
+    public float FireTime = 3.0f;
+
+    private static float kFlashLength = 0.1f;   // just a small fraction of a second.
+
+    public bool flash = false;
+
+    void Awake()
+    {
+        MuzzleFlash1 = GameObject.Find("muzzleflash1");
+        MuzzleFlash2 = GameObject.Find("muzzleflash2");
+
+        SetMuzzleFlashActive(false);
+    }
+    // Use this for initialization
+    void Start () {
+
+        flash = false;
+
+        StartCoroutine("DoTurnOffMuzzleFlash");
+    }
+
+    IEnumerator DoTurnOffMuzzleFlash()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        SetMuzzleFlashActive(false);
+    }
+    
+    // Update is called once per frame
+    void Update () {
+
+        if (!flash)
+        {
+            curTime += Time.deltaTime;
+
+            if (curTime > FireTime)
+            {
+                curTime = 0.0f;
+                curFlashTime = 0.0f;
+
+                flash = true;
+
+                SetMuzzleFlashActive(true);
+            }
+        }
+        else
+        {
+            curFlashTime += Time.deltaTime;
+
+            if (curFlashTime > kFlashLength)
+            {
+                curTime = 0.0f;
+                curFlashTime = 0.0f;
+
+                SetMuzzleFlashActive(false);
+
+                flash = false;
+            }
+        }
+    }
+
+    void SetMuzzleFlashActive(bool active)
+    {
+        if (MuzzleFlash1 != null && MuzzleFlash1.activeSelf != active)
+            MuzzleFlash1.SetActive(active);
+
+        if (MuzzleFlash2 != null && MuzzleFlash2.activeSelf != active)
+            MuzzleFlash2.SetActive(active);
+    }
+}
