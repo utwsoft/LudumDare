ddc53a52770f5b3ed15e034370cfed2bacba88fc
a_a <aa@undertheweathersoftware.com>
2018-04-21 08:26:53 -0700

    more textures etc

---
+++ LD41/LD41/Assets/Card.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour {
+
+    public enum CardValue
+    {
+        Unknown,
+        Ammo,
+        Health,
+        Poison,
+        Poison2,
+        Block
+    }
+
+    public CardValue Value;
+
+    public bool Revealed = false;
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        
+    }
+}
--- LD41/LD41/Assets/CardManager.cs
+++ LD41/LD41/Assets/CardManager.cs
@@ -17,8 +17,15 @@ public class CardManager : MonoBehaviour
     public Texture2D Question;
     public Texture2D Ammo;
     public Texture2D Health;
+    public Texture2D Poison;
+    public Texture2D Poison2;
 
     public SelectionState State;
+
+    public Card.CardValue FirstValue = Card.CardValue.Unknown;
+    public Card.CardValue SecondValue = Card.CardValue.Unknown;
+
+    //private 
 
     private void Awake()
     {
@@ -49,6 +56,38 @@ public class CardManager : MonoBehaviour
                 {
                     Debug.Log("hit: " + obj.name);
 
+                    Card card = obj.GetComponent<Card>();
+                    if (card != null && !card.Revealed)
+                    {
+                        if (FirstValue == Card.CardValue.Unknown && SecondValue == Card.CardValue.Unknown)
+                        {
+
+                                FirstValue = card.Value;
+                                SetTexture(obj, GetTextureForCardValue(card.Value));
+                            card.Revealed = true;
+
+
+                        }
+                        else if (FirstValue != Card.CardValue.Unknown && SecondValue == Card.CardValue.Unknown)
+                        {
+
+                                SecondValue = card.Value;
+                                SetTexture(obj, GetTextureForCardValue(card.Value));
+                            card.Revealed = true;
+
+
+                            // Start a reveal timer. for evaluation
+                        }
+                        else
+                        {
+                            ResetAll();
+                        }
+                    }
+
+
+
+
+
                     switch (State)
                     {
                         case SelectionState.None:
@@ -70,12 +109,77 @@ public class CardManager : MonoBehaviour
 
     }
 
-    private void ResetAll()
+    private Texture2D GetTextureForCardValue(Card.CardValue cardValue)
+    {
+        switch(cardValue)
+        {
+            case Card.CardValue.Unknown:
+                return Question;
+                break;
+            case Card.CardValue.Ammo:
+                return Ammo;
+            case Card.CardValue.Health:
+                return Health;
+            case Card.CardValue.Poison:
+                return Poison;
+            case Card.CardValue.Poison2:
+                return Poison2;
+            default:
+                return Question;
+        }
+    }
+
+    private void ResetImages()
     {
         foreach (GameObject obj in Cards)
         {
             SetTexture(obj, Question);
         }
+    }
+
+    private void InitCardValues()
+    {
+        //Random rnd = new Random();
+        //string[] MyRandomArray = MyArray.OrderBy(x => rnd.Next()).ToArray();
+
+        List<Card.CardValue> values = new List<Card.CardValue>() {
+            Card.CardValue.Ammo,
+            Card.CardValue.Ammo,
+            Card.CardValue.Health,
+            Card.CardValue.Health,
+            Card.CardValue.Poison,
+            Card.CardValue.Poison,
+            Card.CardValue.Poison2,
+            Card.CardValue.Poison2,
+            Card.CardValue.Block,
+        };
+
+        List<Card.CardValue> jumbledValues = new List<Card.CardValue>();
+
+        while (values.Count > 0)
+        {
+
+            int idx = Random.Range(0, values.Count);
+            jumbledValues.Add(values[idx]);
+
+            values.Remove(values[idx]);
+        }
+
+        int index = 0;
+        foreach (GameObject c in Cards)
+        {
+            Card card = c.GetComponent<Card>();
+            card.Value = jumbledValues[index];
+            card.Revealed = false;
+
+            ++index;
+        }
+    }
+
+    private void ResetAll()
+    {
+        ResetImages();
+        InitCardValues();
     }
 
     private void SetTexture(GameObject obj, Texture2D tex)
