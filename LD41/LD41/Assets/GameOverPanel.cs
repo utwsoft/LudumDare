f6cd095eee64cbe50ca8aee55042a9fdb9c61e46
a_a <aa@undertheweathersoftware.com>
2018-04-22 17:21:30 -0700

    Some work on game over. Text update, delayed display

---
+++ LD41/LD41/Assets/GameOverPanel.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverPanel : MonoBehaviour {
+
+    public GameObject GameOverText;
+    public GameObject RestartButton;
+
+    public AudioSource Audio;
+    public AudioClip Sound;
+
+
+    private float curTime = 0.0f;
+
+    public float GameOverRevealTime = 3.0f;
+
+    public float RestartButtonRevealTime = 3.7f;
+
+    void Awake()
+    {
+        GameOverText.SetActive(false);
+        RestartButton.SetActive(false);
+    }
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        curTime += Time.deltaTime;
+
+        if (curTime > GameOverRevealTime)
+        {
+
+            if (!GameOverText.activeSelf)
+            {
+                if (Audio != null && Sound != null)
+                {
+                    Audio.PlayOneShot(Sound);
+                }
+
+                GameOverText.SetActive(true);
+            }
+
+        }
+
+        if (curTime > RestartButtonRevealTime)
+        {
+            RestartButton.SetActive(true);
+        }
+
+
+    }
+
+    public void ResetPanel()
+    {
+        curTime = 0.0f;
+
+        GameOverText.SetActive(false);
+        RestartButton.SetActive(false);
+    }
+}
