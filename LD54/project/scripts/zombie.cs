6c975240c1c1b91b10d813f19b8981ccae04280a
utwsoft <aa@undertheweathersoftware.com>
2023-10-01 10:53:28 -0700

    Add sprite death anim seq, add collision capsule

--- LD54/project/scripts/zombie.cs
+++ LD54/project/scripts/zombie.cs
@@ -8,7 +8,7 @@ public partial class zombie : AnimatedSprite2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		this.Play();
+		this.Play("default");
 		SetLit(false);
 	}
 
