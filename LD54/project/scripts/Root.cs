79e45ef8790248d90066f527cfcc722bfcb08139
utwsoft <aa@undertheweathersoftware.com>
2023-09-30 22:05:56 -0700

    Add zombie, spawn multiple with mat instances, lighting tests

---
+++ LD54/project/scripts/Root.cs
@@ -0,0 +1,60 @@
+using Godot;
+using System;
+public partial class Root : Node2D
+{
+    private RandomNumberGenerator _rng;
+    private PackedScene _zombieScene;
+
+    // Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		_rng = new RandomNumberGenerator();
+
+        _zombieScene = GD.Load<PackedScene>("res://prefabs/Zombie.tscn");
+
+        for (int i = 0; i < 10; ++i)
+        {
+            CreateZombie(_rng.RandiRange(0, 480), _rng.RandiRange(0, 860));
+        }
+    }
+
+	private void CreateZombie(int x, int y)
+	{
+        if (_zombieScene == null)
+            return;
+
+        var node = _zombieScene.Instantiate();
+
+        node.Name = "zombie";
+
+        Node2D monsters = GetNode<Node2D>("monsters");
+        if (monsters != null)
+        {
+            monsters.AddChild(node);
+
+
+            Sprite2D sprite = node as Sprite2D;
+            if (sprite != null)
+            {
+                sprite.Position = new Vector2(x, y);
+            }
+
+            zombie z = node as zombie;
+            if (z != null)
+            {
+                Material mat = GD.Load<Material>("res://materials/MonsterMat.material");
+                if (mat != null)
+                {
+                    var matInstance = mat.Duplicate();
+                    z.Material = matInstance as Material;
+                    z.SetLit(false);
+                }
+            }
+        }
+    }
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
---
+++ LD54/project/scripts/zombie.cs
@@ -0,0 +1,38 @@
+using Godot;
+
+public partial class zombie : Sprite2D
+{
+	float count = 0.0f;
+	bool on = false;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		SetLit(false);
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		//float fDelta = (float)delta;
+		//count += fDelta;
+
+		//if (count >= 1.0f)
+		//{
+		//	count = 0.0f;
+
+		//	on = !on;
+
+		//	SetLit(on);
+		//}
+	}
+
+	public void SetLit(bool isLit)
+	{
+        var mat = this.Material as CanvasItemMaterial;
+        if (mat != null)
+        {
+            mat.LightMode = isLit ? CanvasItemMaterial.LightModeEnum.Unshaded : CanvasItemMaterial.LightModeEnum.Normal;
+        }
+    }
+}
